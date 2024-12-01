ed8ca2eda42cbb727e8041fd032a69527b33fcef
Venera <venii13@icloud.com>
2024-12-01 23:59:15 +0200

    add css and html

--- project-defense/src/app/app.component.ts
+++ project-defense/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BlogComponent } from './blog/blog.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent,
+    BlogComponent,
+    CreatePostComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    PostDetailsComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project-defense';
